6fec2de9aee87e973fed27a32dc9afc3f6211fe6
Joe Kagumba <63820705+JRKagumba@users.noreply.github.com>
2022-09-26 16:00:54 -0400

    Update detect.py
    imported wrong function

--- detect.py
+++ detect.py
@@ -13,7 +13,7 @@ from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
-from utils.plots import colors, plot_one_box, output_to_keypoint
+from utils.plots import colors, plot_one_box, output_to_target
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
